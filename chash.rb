2d2a44abfa304ca101dad12ff92bab500ab73b3a
Olivier Poitrey <rs@dailymotion.com>
2012-03-02 19:53:17 +0100

    Add chash lib with PECL ext

---
+++ chash.rb
@@ -0,0 +1,28 @@
+require 'formula'
+
+class Chash < Formula
+  homepage 'https://github.com/dailymotion/chash'
+  url 'https://github.com/dailymotion/chash/tarball/1.0.1'
+  md5 '24eb0e3748d50b3d62efd6841703ccb7'
+
+  depends_on "libtool"
+  depends_on "automake"
+  depends_on "pkg-config"
+
+  def install
+    cd "libchash" do
+      ENV["LIBTOOLIZE"] = "glibtoolize"
+      system "./bootstrap.sh"
+      system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                            "--prefix=#{prefix}"
+      system "make install"
+    end
+
+    cd "php" do
+      system "phpize"
+      system "./configure", "--prefix=#{prefix}"
+      system "make"
+      prefix.install 'modules/chash.so'
+    end
+  end
+end
