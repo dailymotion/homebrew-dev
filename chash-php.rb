11f05b8a8d534fb5a42a3c83a51fb300046ecc15
Olivier Poitrey <rs@dailymotion.com>
2012-03-06 12:28:48 +0100

    Split chash lib and PECL in two packages

--- chash-php.rb
+++ chash-php.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Libchash < Formula
+class ChashPhp < Formula
   homepage 'https://github.com/dailymotion/chash'
   url 'https://github.com/dailymotion/chash/tarball/1.0.1'
   md5 '24eb0e3748d50b3d62efd6841703ccb7'
@@ -8,14 +8,14 @@ class Libchash < Formula
   depends_on "libtool"
   depends_on "automake"
   depends_on "pkg-config"
+  depends_on "libchash"
 
   def install
-    cd "libchash" do
-      ENV["LIBTOOLIZE"] = "glibtoolize"
-      system "./bootstrap.sh"
-      system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                            "--prefix=#{prefix}"
-      system "make install"
+    cd "php" do
+      system "phpize"
+      system "./configure", "--prefix=#{prefix}"
+      system "make"
+      prefix.install 'modules/chash.so'
     end
   end
 end
