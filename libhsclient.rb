12d4350552247582b94d123e71d1b9c3beb99ec8
Olivier Poitrey <rs@dailymotion.com>
2012-06-18 18:34:36 +0200

    Fix automake bug under lion/mountain lion

--- libhsclient.rb
+++ libhsclient.rb
@@ -9,6 +9,10 @@ class Libhsclient < Formula
   depends_on "automake"
   depends_on "autoconf"
 
+  def patches
+    return [DATA]
+  end
+
   def install
     ENV["LIBTOOLIZE"] = "glibtoolize"
     system "./autogen.sh"
@@ -17,3 +21,17 @@ class Libhsclient < Formula
     system "make install"
   end
 end
+
+__END__
+diff --git a/configure.ac b/configure.ac
+index 202f539..1db2ae7 100644
+--- a/configure.ac
++++ b/configure.ac
+@@ -4,7 +4,7 @@
+ #AC_PREREQ([2.63b])
+ AC_INIT([handlersocket-plugin], [1.0.6], [https://github.com/ahiguti/HandlerSocket-Plugin-for-MySQL
+ AC_CONFIG_HEADERS([config.h])
+-AM_INIT_AUTOMAKE([-Wall -Werror foreign])
++AM_INIT_AUTOMAKE([-Wall -Wno-extra-portability -Werror foreign])
+ AC_CONFIG_SRCDIR([libhsclient/fatal.cpp])
+ AC_CONFIG_MACRO_DIR([m4])
