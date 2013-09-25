e748acc1ea77f014e934173089a4118d458bf6ea
Olivier Poitrey <rs@dailymotion.com>
2013-09-25 15:24:02 -0700

    Fix source URL

--- handlersocket-php.rb
+++ handlersocket-php.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class HandlersocketPhp < Formula
   homepage 'http://code.google.com/p/php-handlersocket/'
-  url 'http://php-handlersocket.googlecode.com/files/php-handelrsocket-0.3.1.tar.gz'
+  url 'https://php-handlersocket.googlecode.com/files/php-handlersocket-0.3.1.tar.gz'
   md5 '87689883a01fd9bc8b0467c0bd96cf6a'
 
   depends_on 'libhsclient'
