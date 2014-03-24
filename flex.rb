f90c3510b39ee7ac92d75b5ec7de6d3472544537
Olivier Audard <audard@gmail.com>
2014-03-24 18:24:30 +0100

    Update flex Formula to v4.12.0

--- flex.rb
+++ flex.rb
@@ -1,26 +1,31 @@
 require 'formula'
 
 class Flex < Formula
-  url 'https://github.com/downloads/dailymotion/homebrew-dev/flex_4.5.0.19786.zip'
-  homepage 'http://opensource.adobe.com/wiki/display/flexsdk/Flex+SDK'
-  md5 '75ca478f61ca5dcc3a861e8af2026562'
+  url 'http://mir2.ovh.net/ftp.apache.org/dist/flex/4.12.0/binaries/apache-flex-sdk-4.12.0-bin.tar.gz'
+  homepage 'http://flex.apache.org/download-binaries.html'
+  sha1 '59dbd6c3d0baa8829e4c6101cd1166e85d2a3c0a'
+  version 'v4.12.0'
 
   def install
-    libexec.install Dir['*']
+    Dir['*'].each { |file| cp_r file, File.join( prefix, File.basename(file) ) }
   end
 
-  def caveats; <<-EOS.undent
+
+  def caveats;
+    s= <<-EOS.undent
     To use the SDK you will need to:
 
     (a) add the bin folder to your $PATH:
-      #{libexec}/bin
+      export PATH=$(brew --prefix flex)/bin:$PATH
 
     (b) set $FLEX_HOME:
-      export FLEX_HOME=#{libexec}
+      export FLEX_HOME=$(brew --prefix flex)
 
     (c) add the tasks jar to ANT:
       mkdir -p ~/.ant/lib
-      ln -s #{libexec}/ant/lib/flexTasks.jar ~/.ant/lib
+      ln -s $(brew --prefix flex)/ant/lib/flexTasks.jar ~/.ant/lib
+
     EOS
+    s
   end
 end
