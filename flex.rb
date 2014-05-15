dde8d6c952cde53a1a7a006e4c4ca2cdbfe546de
Rémy Coutable <remy@rymai.me>
2014-05-15 15:09:13 +0200

    Update flex (4.12.1) and playerglobal (13.0) versions

--- flex.rb
+++ flex.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Flex < Formula
-  url 'http://mir2.ovh.net/ftp.apache.org/dist/flex/4.12.0/binaries/apache-flex-sdk-4.12.0-bin.tar.gz'
+  url 'http://mir2.ovh.net/ftp.apache.org/dist/flex/4.12.1/binaries/apache-flex-sdk-4.12.1-bin.tar.gz'
   homepage 'http://flex.apache.org/download-binaries.html'
-  sha1 '59dbd6c3d0baa8829e4c6101cd1166e85d2a3c0a'
-  version '4.12.0'
+  sha1 '9d14529a607cd8260f4b37557f8989307fa5e7b9'
+  version '4.12.1'
 
   option 'with-playerglobal', "Also installs playerglobal.swc"
 
@@ -12,21 +12,19 @@ class Flex < Formula
     Dir['*'].each { |file| cp_r file, File.join( prefix, File.basename(file) ) }
 
     if build.with? 'playerglobal'
-
-      (prefix/'framework/libs/player/12.0').mkpath
+      (prefix/'framework/libs/player/13.0').mkpath
       resource("flash-playerglobal").fetch
-      (prefix/'frameworks/libs/player/12.0').install resource("flash-playerglobal").cached_download => 'playerglobal.swc'
+      (prefix/'frameworks/libs/player/13.0').install resource("flash-playerglobal").cached_download => 'playerglobal.swc'
 
       inreplace (prefix/'frameworks/flex-config.xml') do |s|
-        s.gsub! /<target-player>11.1<\/target-player>/, "<target-player>12.0</target-player>"
+        s.gsub! /<target-player>11.1<\/target-player>/, "<target-player>13.0</target-player>"
       end
     end
   end
 
   resource "flash-playerglobal" do
-    url 'http://download.macromedia.com/get/flashplayer/updaters/12/playerglobal12_0.swc'
-    sha1 '23c04ac02e61e5b44806749060abe39bf79536a9'
-    version '12.0'
+    url 'http://download.macromedia.com/get/flashplayer/updaters/13/playerglobal13_0.swc'
+    version '13.0'
   end
 
 
